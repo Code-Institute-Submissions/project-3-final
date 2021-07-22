e1aa74a2236c93e4297ee368bb51ded88b2ca5c0
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-07-22 16:27:38 +0000

    finish basic game for battleships

--- run.py
+++ run.py
@@ -10,6 +10,15 @@ def crete_random_ship():
     return random.randint(0, 5), random.randint(0, 5)
 
 
+def play_again():
+    try_again = input("Wanna play again <Y>es or <N>o :").lower()
+    if try_again == "y":
+        play_game()
+    else:
+        print("Suit Yourself!")
+        return
+
+
 print("""Welcome to Battleships!!
 \n Your objective is simple. Find and destroy all three ships!\n""")
 
@@ -22,7 +31,57 @@ For each ship you destroy you will be awarded 1 new bullet
 Good Luck and have fun""")
 
 
-def game():
-    game_board = [["O", "O", "O", "O", "O"],  ["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O"]]
+def play_game():
+    game_board = [["O", "O", "O", "O", "O"],
+                  ["O", "O", "O", "O", "O"],
+                  ["O", "O", "O", "O", "O"],
+                  ["O", "O", "O", "O", "O"],
+                  ["O", "O", "O", "O", "O"]]
+
     for i in game_board:
         print(*i)
+
+    ship1 = crete_random_ship()
+    ship2 = crete_random_ship()
+    ship3 = crete_random_ship()
+    ships_left = 3
+    bullets = 15
+    while bullets:
+        try:
+            row = int(input("Choose a row between 1 and 5: "))
+            column = int(input("Choose a column between 1 and 5: "))
+        except ValueError:
+            print("Numbers only please: ")
+            continue
+
+        if row not in range(1, 6) or column not in range(1, 6):
+            print("That's not between 1 and 5, Try again!: ")
+            continue
+
+        row = row - 1
+        column = column - 1
+
+        if game_board[row][column] == "-" or game_board[row][column] == "X":
+            print("You already shot there!: ")
+            continue
+        elif (row, column) == ship1 or (row, column) == ship2 or (row, column) == ship3:
+            print("Boom! You hit a ship. Here is one extra bullet!")
+            game_board[row][column] = "X"
+            ships_left -= 1
+            if ships_left == 0:
+                print("You Won. Woo Hoo!!!!")
+                play_again()
+        else:
+            print("Missed")
+            game_board[row][column] = "-"
+            bullets -= 1
+
+        for i in game_board:
+            print(*i)
+
+        print(f"Bullets left: {bullets} | Ships left: {ships_left}")
+    play_again()
+
+
+if __name__ == "__main__":
+    play_game()
