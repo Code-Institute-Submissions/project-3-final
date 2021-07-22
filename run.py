7cbf3fff527a3691356cb6dd4c50c083285a85d4
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-07-22 15:59:46 +0000

    add game board

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 import random
 
+# Legend
+# Miss = -
+# Hit = X
+# Game Spaces = O
+
 
 def crete_random_ship():
     return random.randint(0, 5), random.randint(0, 5)
@@ -15,3 +20,9 @@ To shoot, select a row and a collumn between 1 and 5.
 If you hit a ship, you will know about it!!"
 For each ship you destroy you will be awarded 1 new bullet
 Good Luck and have fun""")
+
+
+def game():
+    game_board = [["O", "O", "O", "O", "O"],  ["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O"]]
+    for i in game_board:
+        print(*i)
